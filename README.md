204a5257f0246c1a06fd92c341ae9c21aedaabc7
Fernanda2Olan <al20760686@ite.edu.mx>
2025-02-12 19:36:55 -0800

    Cuadro 3D

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Inteligencia-Artificial
 
-## Inteligencia Artificial - 2025
+## Inteligencia Artificial - 2025 :file_folder:
 En este repositorio están todas las tareas/actividades de la materia de Inteligencia Artificial 2025 - 1
--- Tortuga/main.py
+++ Tortuga/main.py
@@ -2,22 +2,30 @@ from tkinter import *
 
 import turtle  
 pasos = turtle.Turtle() 
+pasos.color("Purple")
+pasos.speed(3)
   
 for i in range(1): 
+
     #Primer cuadro
     pasos.forward(100) 
-    pasos.right(90) 
-    pasos.forward(100)
-    pasos.right(90)
-    pasos.forward(100) 
-    pasos.right(90) 
+    pasos.left(90) 
     pasos.forward(100)
     pasos.left(90)
-    pasos.forward(0) 
+    pasos.forward(100) 
+    pasos.left(90) 
+    pasos.forward(100)
+    pasos.left(90)
+    
+    
     # Segunda parte
-    pasos.left(45) 
-    pasos.forward(50)
-    pasos.left(90)
+    #PenUp para levantar el lápiz, PenDown para bajar el lápiz, 
+    # Goto coordenadas para mover el lápiz a una posició
+    pasos.penup()
+    pasos.goto(50,50)
+    pasos.pendown()
+
+    #Repetimos el primer cuadro
     pasos.forward(100) 
     pasos.left(90) 
     pasos.forward(100)
@@ -26,5 +34,23 @@ for i in range(1):
     pasos.left(90) 
     pasos.forward(100)
     pasos.left(90)
+
+    #Conectar vertices
+    pasos.penup()
+    pasos.goto(0,0)
+    pasos.pendown()
+    pasos.goto(50,50)
+    pasos.penup()
+    pasos.goto(100,0)
+    pasos.pendown()
+    pasos.goto(150,50)
+    pasos.penup()
+    pasos.goto(100,100)
+    pasos.pendown()
+    pasos.goto(150,150)
+    pasos.penup()
+    pasos.goto(0,100)
+    pasos.pendown()
+    pasos.goto(50,150)
     
 turtle.done()
